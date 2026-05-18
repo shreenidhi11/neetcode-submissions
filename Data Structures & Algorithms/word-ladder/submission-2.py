5335bb7e34fad1885aeac43da338bea7a1d40afc
Shreenidhi Vittala Acharya <43963416+shreenidhi11@users.noreply.github.com>
2026-05-17 21:53:14 -0400

    Add: word-ladder - submission-2

---
+++ Data Structures & Algorithms/word-ladder/submission-2.py
@@ -0,0 +1,41 @@
+class Solution:
+    def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
+        # mapping pattern -> words that differ by 1 character
+        queue = deque([beginWord])
+        wordList.append(beginWord)
+
+
+        # check if the endWord is present in the wordlist before the algorithm, because this will avoid unnecessary BFS traversal
+        if endWord not in wordList:
+            return 0 
+            
+        pattern_dict = defaultdict(list)
+        for word in wordList:
+            for index in range(len(word)):
+                pattern = word[:index] + "*" + word[index + 1:]
+                pattern_dict[pattern].append(word)
+        
+        visit = set([beginWord])
+        sequence_length = 1
+
+        while queue:
+            for _ in range(len(queue)):
+                current_word = queue.popleft()
+                if current_word == endWord:
+                    return sequence_length
+                
+                for index in range(len(current_word)):
+                    pattern = current_word[:index] + "*" + current_word[index + 1:]
+                    for neighbours in pattern_dict[pattern]:
+                        if neighbours not in visit:
+                            queue.append(neighbours)
+                            visit.add(neighbours)
+            sequence_length += 1
+
+        
+        return 0
+
+                                
+
+
+        
